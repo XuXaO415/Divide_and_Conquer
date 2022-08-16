534e5db1b2ff15992f6dff211ffe9578c8ee3013
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-08-15 21:30:35 -0700

    Added clarification to line 16

--- Divide_and_conquer_exercise/count-zeroes.js
+++ Divide_and_conquer_exercise/count-zeroes.js
@@ -2,13 +2,18 @@
 // Since we have a constraint with a time complexity of O(log n), we can use binary search to find the number of zeros in the array.
 
 function countZeroes(arr) {
-    //leftIdx is where start our array search
+    // leftIdx is where start our array search
     let leftIdx = 0;
+    // rightIdx is where we end our array search
     let rightIdx = arr.length - 1;
 
+    // while leftIdx is less than or equal to rightIdx keep searching
     while (leftIdx <= rightIdx) {
+        // find the midpoint of the array by taking the average of the leftIdx and rightIdx
         let midIdx = Math.floor((leftIdx + rightIdx) / 2);
-        if (arr[leftIdx] === 0) {
+        // if (arr[leftIdx] === 0) {
+        // if the midpoint and leftIdx are equal to 0, return the length of the array minus the leftIdx
+        if (arr[midIdx && leftIdx] === 0) {
             // return arr.length - 1;
             return arr.length - leftIdx;
         } else if (arr[midIdx] === 1) {
