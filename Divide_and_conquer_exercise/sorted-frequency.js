2a31b1578dfc26ea24ef3d532226658f5e1c2276
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-08-16 21:29:23 -0700

    Still tyring to figure this one out

--- Divide_and_conquer_exercise/sorted-frequency.js
+++ Divide_and_conquer_exercise/sorted-frequency.js
@@ -1,5 +1,37 @@
-function sortedFrequency() {
+//Given a sorted array and a number, write a function called sortedFrequency that counts the occurrences of the number in the array
 
+// Constraints: Time Complexity: O(log N)
+
+
+function sortedFrequency(arr, target) {
+    let leftIdx = 0;
+    let rightIdx = arr.length - 1;
+
+
+    while (leftIdx <= rightIdx) {
+        let mid = Math.floor((leftIdx + rightIdx) / 2);
+        if (arr[mid] < target) {
+            leftIdx = mid + 1;
+        } else if (arr[mid] > target) {
+            rightIdx = mid - 1;
+        } else {
+            if (arr[mid - 1] < target) {
+                return mid;
+            } else {
+                rightIdx = mid - 1;
+            }
+        }
+    }
+    return -1;
 }
 
-module.exports = sortedFrequency
+
+
+
+console.log(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 2)); // 4
+console.log(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 3)); // 1
+console.log(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 1)); // 2
+console.log(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 4)); // -1
+
+
+module.exports = sortedFrequency;
